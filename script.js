75eb1f3a4ecb69a6e64bcaf63cb3965ccac1d476
teshager <teshager8922@gmail.com>
2024-06-26 21:21:02 +0300

    use a create card function to make the code readable

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ const myLibrary=[
         numberOfPages:"384 pages"
     },
     {title:"The Housemaid Is Watching",
-        author:"The Housemaid Is Watching",
+        author:"Freida McFadden",
         numberOfPages:"400 pages"
     },
     {title:"The Women: A Novel",
@@ -21,35 +21,40 @@ function Book(title,author, numberOfPages){
 
 }
 
+const book_cards=document.querySelector('.book-cards');
+
 //add new book
 function addBooktoLibrary(){
 
 }
+const createCard=(title,author,numberOfPages)=>{
+    const book_card=document.createElement('div');
+    const book_title_El=document.createElement('h3');
+    const author_El= document.createElement('p');
+    const number_Of_Pages_El= document.createElement('p');
 
-function displayBooks(){
-    myLibrary.map(book=>{
-        const {title,author,numberOfPages}=book;
-        
-        const book_cards =document.querySelector(".book-cards");
-        const book_card=document.createElement('div');
+//adding classes
         book_card.classList.add('card');
-        const book_title_El=document.createElement('h3')
         book_title_El.classList.add('book-title');
-        book_title_El.textContent=title;
-        const author_El= document.createElement('p');
         author_El.classList.add("author");
-        author_El.textContent=author;
-        const number_Of_Pages_El= document.createElement('p');
         number_Of_Pages_El.classList.add('numberOfPages')
-        number_Of_Pages_El.textContent=numberOfPages;
-
+//appending elements to the card
         book_card.appendChild(book_title_El);
         book_card.appendChild(author_El);
         book_card.appendChild(number_Of_Pages_El);
+//populate with content
+        book_title_El.textContent=title;
+        author_El.textContent=author;
+        number_Of_Pages_El.textContent=numberOfPages;    
+        
 
+    return book_card;
 
-
-        // book_title_El.textContent=title;    
+}
+function displayBooks(){
+    myLibrary.map(book=>{
+        const {title,author,numberOfPages}=book;
+        const book_card=createCard(title,author,numberOfPages);
         book_cards.appendChild(book_card);
     })
 }
